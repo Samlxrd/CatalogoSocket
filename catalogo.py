6d99d237e42d447df9a040f12d77b21e02dee1ca
Samlxrd <samlord.dzn@gmail.com>
2023-11-30 13:30:31 -0300

    added favorites querys

--- catalogo.py
+++ catalogo.py
@@ -48,7 +48,7 @@ class Catalogo:
     def _criar_tipos(self):
         # Faz os inserts dos tipos disponiveis
         self.insert_type('Filme')
-        self.insert_type('Serie')
+        self.insert_type('Série')
         self.insert_type('Anime')
         self.insert_type('Outro')
 
@@ -64,6 +64,41 @@ class Catalogo:
             INSERT INTO item (itemname, itemtype)
             VALUES (?, ?)
         ''', (itemname, itemtype))
+        self.conn.commit()
+
+    def insert_user(self, username):
+        self.c.execute('''
+            INSERT INTO user (username)
+            VALUES (?)
+        ''', (username,))
+        self.conn.commit()
+
+    def get_user_id(self, username):
+        self.c.execute('''
+            SELECT userid FROM user
+            WHERE username=?
+        ''', (username,))
+        return self.c.fetchall()
+
+    def insert_favorites(self, user_id, item_id):
+        self.c.execute('''
+            INSERT INTO favs (favuser, favitem)
+            VALUES(?, ?)
+        ''', (user_id, item_id))
+        self.conn.commit()
+
+    def was_favorited(self, user_id, item_id):
+        self.c.execute('''
+            SELECT favid from favs
+            WHERE favuser = ? AND favitem = ?
+        ''', (user_id, item_id))
+        return self.c.fetchall()
+
+    def delete_favorites(self, fav_id):
+        self.c.execute('''
+            DELETE FROM favs
+            WHERE favid=?
+        ''', (fav_id,))
         self.conn.commit()
 
     def update_item(self, itemid, itemname, itemtype):
@@ -94,6 +129,17 @@ class Catalogo:
             SELECT type.typeid, type.typename FROM type
         ''')
         return self.c.fetchall()
+    
+    def query_favorites(self, user_id):
+        self.c.execute('''
+            SELECT favs.favid, item.itemname, type.typename
+            FROM favs
+            INNER JOIN item ON favs.favitem = item.itemid
+            INNER JOIN type on item.itemtype = type.typeid
+            WHERE favs.favuser=?
+        ''', (user_id,))
+        return self.c.fetchall()
+
 
     def search_items(self, searchname: str):
         self.c.execute('''
@@ -101,6 +147,16 @@ class Catalogo:
             FROM item INNER JOIN type ON item.itemtype = type.typeid
             WHERE item.itemname LIKE ? ORDER BY item.itemname asc
         ''', ('%' + searchname + '%',))
+        return self.c.fetchall()
+    
+    def search_favorites(self, user_id, searchname: str):
+        self.c.execute('''
+            SELECT favs.favid, item.itemname, type.typename
+            FROM favs
+            INNER JOIN item ON favs.favitem = item.itemid
+            INNER JOIN type on item.itemtype = type.typeid
+            WHERE favs.favuser=? and item.itemname LIKE ? ORDER BY item.itemname asc
+        ''', (user_id, '%' + searchname + '%',))
         return self.c.fetchall()
 
     def close_connection(self):
